d167893fb03f399c8a27279833e85b9c35aa2178
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-31 14:08:53 +0200

    docstr

--- aquordion/conversion.py
+++ aquordion/conversion.py
@@ -27,6 +27,14 @@ pyq_to_horqrux_types_match = {
 
 
 def pyqcircuit_to_horqrux(qc: pyq.QuantumCircuit) -> horqrux.QuantumCircuit:
+    """Convert a QuantumCircuit from pyqtorch to an horqrux equivalent.
+
+    Args:
+        qc (pyq.QuantumCircuit): pyqtorch QuantumCircuit instance.
+
+    Returns:
+        horqrux.QuantumCircuit: horqrux QuantumCircuit instance
+    """
     horqrux_ops = list()
     for op in qc.flatten():
         call_op = pyq_to_horqrux_types_match[type(op)]
@@ -41,10 +49,28 @@ def pyqcircuit_to_horqrux(qc: pyq.QuantumCircuit) -> horqrux.QuantumCircuit:
 
 
 def jarr_to_tensor(arr: Array, dtype: Any = cdouble) -> Tensor:
+    """Convert a jax array to a torch tensor.
+
+    Args:
+        arr (Array): Jax array.
+        dtype (Any, optional): Data type. Defaults to cdouble.
+
+    Returns:
+        Tensor: torch Tensor conversion.
+    """
     return from_numpy(device_get(arr)).to(dtype=dtype)
 
 
 def tensor_to_jnp(tensor: Tensor, dtype: Any = jnp.complex128) -> Array:
+    """Convert a torch tensor to jax array.
+
+    Args:
+        tensor (Tensor): torch tensor.
+        dtype (Any, optional): Data type. Defaults to jnp.complex128.
+
+    Returns:
+        Array: Jax array conversion.
+    """
     return (
         jnp.array(tensor.numpy(), dtype=dtype)
         if not tensor.requires_grad
@@ -53,4 +79,12 @@ def tensor_to_jnp(tensor: Tensor, dtype: Any = jnp.complex128) -> Array:
 
 
 def values_to_jax(param_values: dict[str, Tensor]) -> dict[str, Array]:
+    """Convert a dictionary of torch Tensor values to dictionary of jax arrays.
+
+    Args:
+        param_values (dict[str, Tensor]): Parameter values with torch tensors.
+
+    Returns:
+        dict[str, Array]: Parameter values with jax arrays.
+    """
     return {key: jnp.array(value.detach().numpy()) for key, value in param_values.items()}
