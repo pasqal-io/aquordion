72cf053e7425583f9376ac71f8eb11da45fe488f
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-04-01 22:02:49 +0200

    fix lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from typing import Callable
-import pytest
 import random
+from typing import Callable
+
+import pytest
 from qadence import CNOT, MCZ, RX, RY, RZ, H, QuantumCircuit, chain
 
 
@@ -55,16 +56,21 @@ def circuit_C(n_qubits: int, n_layers: int = 1) -> tuple[QuantumCircuit, list[st
     circ = QuantumCircuit(n_qubits, chain(*ops))
     return circ, params
 
+
 @pytest.fixture
 def random_circuit_integers() -> tuple[int, int]:
     n_qubits = random.randint(1, 5)
     n_layers = random.randint(1, 5)
     return n_qubits, n_layers
 
-@pytest.fixture(params=[
-    circuit_A,
-    circuit_B, 
-    circuit_C,
-], ids=["circuit_A", "circuit_B", "circuit_C"])
-def fn_circuit(request) -> Callable: # type: ignore[no-untyped-def]
-    return request.param # type: ignore[no-any-return]
+
+@pytest.fixture(
+    params=[
+        circuit_A,
+        circuit_B,
+        circuit_C,
+    ],
+    ids=["circuit_A", "circuit_B", "circuit_C"],
+)
+def fn_circuit(request) -> Callable:  # type: ignore[no-untyped-def]
+    return request.param  # type: ignore[no-any-return]
