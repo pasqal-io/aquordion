6f8fb278f4caf1d069c7eb9a8fdc3b25f1012907
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-04-02 12:57:05 +0200

    init replace

--- aquordion/__init__.py
+++ aquordion/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-from .benchmark_api import expectation_horqrux, expectation_pyq, run_horqrux, run_pyq
+from .benchmarks import expectation_horqrux, expectation_pyq, run_horqrux, run_pyq
 from .utils import values_to_jnp
