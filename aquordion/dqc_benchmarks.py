42f482ff67d6dedf10ab583b9ff4c77ddc410bc5
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-05-12 13:14:23 +0200

    add dqc for pyq

---
+++ aquordion/dqc_benchmarks.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+from typing import Callable
+
+import horqrux
+import jax
+import optax
+import pyqtorch as pyq
+import torch
+from jax import Array
+from torch import Tensor
+from torch.nn import ParameterDict
+from .vqe_benchmarks import native_expectation_horqrux, native_expectation_pyq
+
+VARIABLES = ("x", "y")
+N_VARIABLES = len(VARIABLES)
+X_POS, Y_POS = [i for i in range(N_VARIABLES)]
+BATCH_SIZE = 250
+
+
+def dqc_pyq_adam(
+    circuit: pyq.QuantumCircuit,
+    observable: pyq.Observable,
+    inputs_embedded: ParameterDict,
+    LR: float = 1e-2,
+    N_epochs: int = 30,) -> Callable:
+
+    def exp_fn(inputs: Tensor) -> Tensor:
+        return native_expectation_pyq(
+            circuit,
+            observable,
+            {**inputs_embedded, **{VARIABLES[X_POS]: inputs[:, X_POS], VARIABLES[Y_POS]: inputs[:, Y_POS]}},
+        )
+    
+    class DomainSampling(torch.nn.Module):
+        def __init__(self) -> None:
+            super().__init__()
+
+        def sample(self, requires_grad: bool = False) -> Tensor:
+            return torch.rand((BATCH_SIZE, N_VARIABLES), requires_grad=requires_grad)
+    
+    
+        def left_boundary(self) -> Tensor:  # u(0,y)=0
+            sample = self.sample()
+            sample[:, X_POS] = 0.0
+            return exp_fn(sample).pow(2).mean()
+
+        def right_boundary(self) -> Tensor:  # u(L,y)=0
+            sample = self.sample()
+            sample[:, X_POS] = 1.0
+            return exp_fn(sample).pow(2).mean()
+
+        def top_boundary(self) -> Tensor:  # u(x,H)=0
+            sample = self.sample()
+            sample[:, Y_POS] = 1.0
+            return exp_fn(sample).pow(2).mean()
+
+        def bottom_boundary(self) -> Tensor:  # u(x,0)=f(x)
+            sample = self.sample()
+            sample[:, Y_POS] = 0.0
+            return (exp_fn(sample) - torch.sin(torch.pi * sample[:, 0])).pow(2).mean()
+
+        def interior(self) -> Tensor:  # uxx+uyy=0
+            sample = self.sample(requires_grad=True)
+            exp_eval = exp_fn(sample)
+            dfdxy = torch.autograd.grad(
+                exp_eval,
+                sample,
+                torch.ones_like(exp_eval),
+                create_graph=True,
+            )[0]
+            dfdxxdyy = torch.autogradgrad(
+                dfdxy,
+                sample,
+                torch.ones_like(dfdxy),
+            )[0]
+
+            return (dfdxxdyy[:, X_POS] + dfdxxdyy[:, Y_POS]).pow(2).mean()
+    
+    d_sampler = DomainSampling()
+
+    def opt_pyq() -> None:
+        optimizer = torch.optim.Adam(inputs_embedded.values(), lr=LR, foreach=False)
+        def loss_fn() -> Tensor:
+            return d_sampler.left_boundary() + d_sampler.right_boundary() + d_sampler.top_boundary() + d_sampler.bottom_boundary() + d_sampler.interior()
+    
+        for _ in range(N_epochs):
+            optimizer.zero_grad()
+            loss = loss_fn()
+            loss.backward()
+            optimizer.step()
+    
+    return opt_pyq
---
+++ tests/timings/dqc/test_noshots.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Callable
+
+import horqrux
+import jax
+import pyqtorch as pyq
+import pytest
+import torch
+from qadence import AbstractBlock, RX, hamiltonian_factory, Z, kron, QuantumCircuit
+from torch.nn import ParameterDict
+
+from aquordion.api_benchmarks import (
+    bknd_horqrux,
+    bknd_pyqtorch,
+)
+from aquordion.dqc_benchmarks import dqc_pyq_adam
+
+def test_dqc_pyq(
+    benchmark: pytest.Fixture,
+    benchmark_dqc_ansatz: tuple[Callable, int, int],
+) -> None:
+    fn_circuit, n_qubits, n_layers = benchmark_dqc_ansatz
+    ansatz = fn_circuit(n_qubits, n_layers)
+    feature_map = kron(*[RX(i, "x") for i in range(n_qubits // 2)] + [
+        RX(i, "y") for i in range(n_qubits // 2, n_qubits)])
+    
+    circuit = QuantumCircuit(n_qubits, feature_map, ansatz)
+    total_magnetization = hamiltonian_factory(n_qubits, detuning=Z)
+    torch.manual_seed(0)
+    values = {p: torch.rand(1, requires_grad=True) for p in circuit.unique_parameters}
+    # avoid multiple conversion
+    (circ, obs, embed_fn, params_conv) = bknd_pyqtorch.convert(circuit, total_magnetization)
+
+    circ = circ.native
+    obs = obs[0].native
+    inputs_embedded = ParameterDict({p: v for p, v in embed_fn(params_conv, values).items()})
+    opt_pyq = dqc_pyq_adam(circ, obs, inputs_embedded, N_epochs=10)
+    benchmark.pedantic(opt_pyq, rounds=5)
+
